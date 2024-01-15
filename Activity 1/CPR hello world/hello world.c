90e0c50316cad3338e5cab9bb80df5fb532165a7
harsahbaj10 <harsahbaj10@gmail.com>
2024-01-15 10:05:26 -0500

    Initial commit

---
+++ Activity 1/CPR hello world/hello world.c
@@ -0,0 +1,18 @@
+/* Thanks to Brian Kernighan, 'Hello World' is the traditional first C program. It became
+legendary with the publication of "The C Programming Language" by Kernighan and Ritchie (1978).
+Now, Hello World is the canonical test of any programming language. */
+/*
+Full Name : Harsahbaj Singh
+Student ID# : 146457221
+UserID / Email : hsna27|hsna27@mySeneca.ca
+*/
+#include <stdio.h>// C language module providing Input/Output facilities
+int main(void) // main() is automatically called to start a C program
+{
+	printf("Hello, World!\n"); // output greeting
+	// Why I came to Seneca ...
+	/* insert printf statements here using code completion */
+	printf("I am Harsahbaj Singh.\n"); //output your name
+	printf("why did you come to seneca?");
+	return 0; // return to operating system
+}
---
+++ Final project/tokenizing.c/tokenizing.c
@@ -0,0 +1,55 @@
+//TOKENIZING MODULE SOURCE 
+#define _CRT_SECURE_NO_WARNINGS 
+#define BUFFER_SIZE 300 
+#include "tokenizing.h"
+
+	// V1
+void tokenizing(void) {
+	printf("**** Start of Tokenizing words Demo ***\n");
+	char  words[BUFFER_SIZE];
+	char* nextWord = NULL;
+	int   wordsCounter;
+	do {
+		printf("Type a few words separated by space (q - to quit):\n");
+		fgets(words, BUFFER_SIZE, stdin);
+
+		words[strlen(words) - 1] = '\0';  // Removes the trailing newline character from words by replacing it with the null terminator
+
+		if (strcmp(words, "q") != 0) {
+			nextWord = strtok(words, " ");
+			wordsCounter = 1;
+			while (nextWord) {
+				printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
+				nextWord = strtok(NULL, " ");
+			}
+
+		}
+	} while (strcmp(words, "q") != 0); // quits the process if q is entered
+	printf("*** End of Tokenizing words Demo ***\n\n");
+
+	/* Version 2 */
+	 //insert here
+	//v2 
+	printf("*** Start of Tokenizing Phrases Demo ***\n");
+	char phrases[BUFFER_SIZE];
+	char* nextPhrase = NULL;
+	int phrasesCounter;
+
+	do {
+		printf("Type a few phrases separated by comma (q to quit):\n");
+		fgets(phrases, BUFFER_SIZE, stdin);
+		phrases[strlen(phrases) - 1] = '\0';
+		if ((strcmp(phrases, "q") != 0)) {
+			nextPhrase=strtok(phrases, ",");
+			phrasesCounter = 1;
+			while (nextPhrase) {
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				nextPhrase = strtok(NULL, ",");
+			}
+		}
+
+	} while (strcmp(phrases, "q") != 0);
+	printf("*** End of Tokenizing Phrases Demo ***\n\n");
+}
+/* Version 3 */
+//insert here
